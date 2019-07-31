1ed1aeda38bda5a105f12a2c0bbbbfa7dd92d77b
ian <ian.miell@gmail.com>
2019-07-31 20:21:21 +0100

    rtf

--- main.tf
+++ main.tf
@@ -1,28 +1,11 @@
 # Configure the Vultr Provider
-
-variable "token" {}
-
 provider "vultr" {
   api_key = "${var.token}"
 }
 
-variable "vultr_bm_region" {
-  description = "Bare metal region"
-  default = 2
-}
-
-variable "os_type" {
-  description = "Application"
-  default = "338"
-}
-
-variable "bare_metal_32g" {
-  description = "bare metal server"
-  default = 100
-}
-
 resource "vultr_bare_metal_server" "my_server" {
-  plan_id = "${var.bare_metal_32g}"
+  plan_id   = "${var.bare_metal_32g}"
   region_id = "${var.vultr_bm_region}"
-  os_id = "${var.os_type}"
+  os_id     = "${var.os_type}"
+  script_id = "563352"
 }
---
+++ vars.tf
@@ -0,0 +1,16 @@
+variable "token" {}
+
+variable "vultr_bm_region" {
+  description = "Bare metal region"
+  default     = 2
+}
+
+variable "os_type" {
+  description = "Application"
+  default     = "338"
+}
+
+variable "bare_metal_32g" {
+  description = "bare metal server"
+  default     = 100
+}
