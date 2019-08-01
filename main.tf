3c7070ef2b1e694f553eaa33780d604e6376fae0
ian <ian.miell@gmail.com>
2019-08-01 09:30:22 +0100

    resource ref

--- main.tf
+++ main.tf
@@ -3,9 +3,15 @@ provider "vultr" {
   api_key = "${var.token}"
 }
 
+resource "vultr_startup_script" "bm_startup" {
+    name = "Update password on bm startup"
+    script = "echo root:vultr | /usr/sbin/chpasswd"
+}
+
 resource "vultr_bare_metal_server" "my_server" {
   plan_id   = "${var.bare_metal_32g}"
   region_id = "${var.vultr_bm_region}"
   os_id     = "${var.os_type}"
-  script_id = "563352"
+  script_id = "${resources.vultr_startup_script.bm_startup.id}"
 }
+
